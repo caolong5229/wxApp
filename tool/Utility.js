21ec6c447a62255587b46fdeeb081090e21d7081
caolong5229 <724303493@qq.com>
2020-04-10 11:30:01 +0800

    【新增】登录验证码

--- tool/Utility.js
+++ tool/Utility.js
@@ -553,5 +553,68 @@ export default class Utility {
       pagedata.push(data[i]) 
     }
     return pagedata ;
-  }  
+  };
+  /*
+    生成验证码
+    字母加数字
+  */
+  static drawPic(canvas,str){
+    /**生成一个随机数**/
+    function randomNum(min,max){
+      return Math.floor( Math.random()*(max-min)+min);
+    }
+    /**生成一个随机色**/
+    function randomColor(min,max){
+      var r = randomNum(min,max);
+      var g = randomNum(min,max);
+      var b = randomNum(min,max);
+      return "rgb("+r+","+g+","+b+")";
+    }
+    var width=canvas.width;
+    var height=canvas.height;
+    var ctx = canvas.getContext('2d');
+    ctx.textBaseline = 'bottom';
+
+    /**绘制背景色**/
+    ctx.fillStyle = randomColor(180,240); //颜色若太深可能导致看不清
+    ctx.fillRect(0,0,width,height);
+    /**绘制文字**/
+    for(var i=0; i<str.length; i++){
+      var txt = str[i];
+      var isDaxie = randomNum(0,2);  //随机生成字体大小写
+      if(isDaxie)txt = txt.toUpperCase();
+      ctx.fillStyle = randomColor(50,160);  //随机生成字体颜色
+      ctx.font = randomNum(35,40)+'px SimHei'; //随机生成字体大小
+      var x = 10+i*30;
+      var y = randomNum(45,50);
+      var deg = randomNum(-10, 10);
+      //修改坐标原点和旋转角度
+      ctx.translate(x,y);
+      ctx.rotate(deg*Math.PI/180);
+      ctx.fillText(txt, 0,0);
+      //恢复坐标原点和旋转角度
+      ctx.rotate(-deg*Math.PI/180);
+      ctx.translate(-x,-y);
+    }
+    /**绘制干扰线**/
+    for(var i=0; i<2; i++){
+      ctx.strokeStyle = randomColor(40,180);
+      ctx.beginPath();
+      ctx.moveTo( randomNum(0,width), randomNum(0,height) );
+      ctx.lineTo( randomNum(0,width), randomNum(0,height) );
+      ctx.stroke();
+    }
+    /**绘制干扰点**/
+    for(var i=0; i<50; i++){
+      ctx.fillStyle = randomColor(0,255);
+      ctx.beginPath();
+      ctx.arc(randomNum(0,width),randomNum(0,height), 1, 0, 2*Math.PI);
+      ctx.fill();
+    }
+  };
+  //获取验证码
+  static getYanZhengma(){
+    let yanzhengCode = '';//通过调接口获取
+    this.drawPic(this.$refs.yanzhengma,yanzhengCode);//ref绑定canvas
+  };
 }
